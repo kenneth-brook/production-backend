a2fe71b152d27110df92a861e3f1ec3fafb30b04
Kenneth Brook <kenneth.brook@hotmail.com>
2021-08-15 17:37:17 -0400

    working example

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('./logger/logger');
+
+const app = express();
+const port = 3070;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  logger.info('default route');
+  res.send('App works!!!!!');
+});
+
+app.use('/api', require('./routes/routes'));
+
+// request to handle undefined or all other routes
+app.get('*', (req, res) => {
+  logger.info('users route');
+  res.send('App works!!!!!');
+});
+
+app.listen(port, (err) => {
+  logger.info(`running server on from port:::::::${port}`);
+});
